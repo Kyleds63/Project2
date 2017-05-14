941df0bf5c938ee873fdfd163e06085b77dc0311
kyleds63 <kstoner63@gmail.com>
2017-05-14 13:09:47 -0500

    Continuing to merge Andrews with my passport

--- server.js
+++ server.js
@@ -2,31 +2,83 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var passport   = require('passport');
+var session    = require('express-session');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3307;
+var PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
+//==============================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//Passport
+//===================================================================
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize()); 
+app.use(passport.session()); // persistent login sessions
+
 // Static directory
+//==================================================================
 app.use(express.static("app/public"));
 
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
 require("./app/routes/html-routes.js")(app);
+var authRoute = require('./app/routes/auth.js')(app,passport);
+
+
+//Models
+//=============================================================
+var models = require("./app/models");
+
+//Load Passport Strategies
+//================================================================
+require('./app/config/passport/passport.js')(passport, models.user);
+
+ 
+//For Handlebars
+//==============================================================
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+
+
+//Sync Database
+//================================================================
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(5000, function(err) {
+	if(!err){
+         console.log("App listening on PORT " + PORT);
+    }else{
+    	console.log(err);
+    }
 });
