c4d6321fa1027bf42e0d81b1dbbde2ea2d4ca8b1
Kyleds63 <kstoner63@gmail.com>
2017-05-15 09:32:14 -0500

    changed server back

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
-var authRoute = require('./routes/auth.js')(app,passport);
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
+var authRoute = require('./app/routes/auth.js')(app,passport);
 
 
 //Models
